75fecc7ada0cf803da7825142f7b8fe1b8436c7d
Vijayaudacious <106574386+Vijayaudacious@users.noreply.github.com>
2022-06-16 11:15:43 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,11 @@
+let x = "This is My first msg"
+console.log(x)
+let y = "This is My 'first' Msg"
+console.log(y)
+document.write(x +" "+ y);
+
+{
+var a = "Vijay Saratkar"
+console.log(a.slice(6,15))
+document.write(a.slice(9,15))
+}
